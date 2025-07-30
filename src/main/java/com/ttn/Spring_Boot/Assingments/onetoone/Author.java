301d73f8b00244846c754dea43e0396bb6264293
Animesh Yadav <animesh.yadav@tothenew.com>
2025-07-30 23:57:28 +0530

    edited some annotatins

--- src/main/java/com/ttn/Spring_Boot/Assingments/onetoone/Author.java
+++ src/main/java/com/ttn/Spring_Boot/Assingments/onetoone/Author.java
@@ -1,5 +1,14 @@
 package com.ttn.Spring_Boot.Assingments.onetoone;
 
-public class Author {
+import com.ttn.Spring_Boot.Assingments.onetomany.Book;
+import jakarta.persistence.*;
 
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int authorId;
+    private String name;
+
+    @OneToOne
+    private Book book;
 }
--- src/main/java/com/ttn/Spring_Boot/Assingments/onetoone/Book.java
+++ src/main/java/com/ttn/Spring_Boot/Assingments/onetoone/Book.java
@@ -1,4 +1,17 @@
 package com.ttn.Spring_Boot.Assingments.onetoone;
 
+import com.ttn.Spring_Boot.Assingments.onetomany.Author;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
 public class Book {
+
+
+    @Id
+    private Long bookId;
+
+    @OneToOne
+    private Author Author;
 }
